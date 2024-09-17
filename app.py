6cedcd01d51c24c71f583047917c02374877c6c6
Maksymilian Babik <101732925+GreenBananana@users.noreply.github.com>
2024-09-17 14:25:37 +0000

    added makeshift job page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -17,5 +17,13 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>") #dynamic route.
+def show_job(id):
+  job = load_job_from_db(id)
+  if job:  
+    return render_template('jobpage.html', job=job)
+  else:
+    return "Job not found", 404
+    
 if __name__ == "__main__":
   app.run(host ='0.0.0.0', debug = True)
--- database.py
+++ database.py
@@ -20,6 +20,15 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as connection:
+  
+    result = connection.execute(text(f"select * from jobs where id = {id}"))
+    row = result.fetchone()
+    if row:
+      return row._asdict()
+    else:
+      return None
 
 
 
