1656cbd7d27d2e6a6471851008dcca0782979bba
Maksymilian Babik <101732925+GreenBananana@users.noreply.github.com>
2024-09-17 13:41:29 +0000

    Added database connection

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title' : 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 2,
-    'title' : 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 12,00,000'
-  },
-  {
-    'id': 3,
-    'title' : 'Backend Engineer',
-    'location': 'Remote',
-  },
-  {
-    'id': 4,
-    'title' : 'Frontend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '150,000$'
-  }
-]
+
 
 @app.route("/")
 def hello_jovian():
+  jobs = load_jobs_from_db()
   return render_template('home.html',
-                        jobs= JOBS)
+                        jobs=jobs) # first jobs is name used later, second jobs is the variable inside this method.
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host ='0.0.0.0', debug = True)
---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+import os #needed for secrets
+
+db_connection_string = os.environ['DB_CONNECTION_STRING'] # don't share this!!!! Make a secret or env variable
+
+engine = create_engine(db_connection_string,pool_use_lifo=True, pool_pre_ping=True) #ssl certificate may be required with different db providers.
+
+with engine.connect() as connection:
+  result = connection.execute(text("select * from jobs"))
+
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(row._asdict())
+
+def load_jobs_from_db():
+  with engine.connect() as connection:
+    result = connection.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+
+
+
+
